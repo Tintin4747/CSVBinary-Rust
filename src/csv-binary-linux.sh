d74b9a810a586cf971d5db3013c4ff8955a8501e
Quentin <31995801+Tintin4747@users.noreply.github.com>
2025-06-02 15:25:02 +0200

    Calcul du temps d'exécution des scripts

--- src/csv-binary-linux.sh
+++ src/csv-binary-linux.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+# Fonction utilitaire pour mesurer la durée
+time_step() {
+    local label="$1"
+    shift
+    start=$(date +%s)
+    local start
+    "$@"
+    local status=$?
+    end=$(date +%s)
+    local end
+    local duration=$((end - start))
+    echo "Durée pour $label : ${duration}s"
+    return $status
+}
+
 # Générer les transactions
-./generate_transactions-linux
+time_step "Génération des transactions (1)" ./generate_transactions-linux
 
 # Vérifier si binsort est installé
 if ! command -v binsort &> /dev/null; then
@@ -13,14 +28,14 @@ fi
 binsort -s 23 -o 17 -l 2 out/transactions.bin out/transactions-filtered.bin
 
 # Générer les soldes des utilisateur
-./balance_by_user-linux
+time_step "Génération des soldes utilisateurs" ./balance_by_user-linux
 
 # Afficher les résultats
 ./read_balances_from_binary-linux
 
 # Générer de nouvelles transactions
-./generate_transactions-linux
+time_step "Génération des transactions (2)" ./generate_transactions-linux
 binsort -s 23 -o 17 -l 2 out/transactions.bin out/transactions-filtered.bin
 
-./update_balance_file-linux
+time_step "Mise à jour des soldes utilisateurs" ./update_balance_file-linux
 ./read_balances_from_binary-linux out/balances_updated.bin
